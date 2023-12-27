d9b8d7bd4b5cbdd61b1bd3ec2ed7a3742609275a
jennmyoon <jenniferminahyoon@gmail.com>
2023-12-26 21:04:03 -0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useEffect } from 'react-router-dom';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import ProjectSummary from './components/projectSummary';
